c9aa24f26024dbf67c6582dc98f2cf8335e8c969
Elia Light <141272836+AKATWIJUKA-ELIA@users.noreply.github.com>
2025-02-27 18:24:34 +0300

    carousel fix

--- Eccommerce/urls.py
+++ Eccommerce/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Ecc.urls')),
+    path('schema-viewer/', include('schema_viewer.urls'))
 ]
 
 if settings.DEBUG:
